f98751cbcc691158c41f3030c2718166be3572f4
Ahmed Sherif <93347305+heyahmedsherif@users.noreply.github.com>
2022-02-28 14:55:22 -0500

    updates with new template

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def home():
-    return 'more to come very soon on ahmedsherif.io'
+def hello():
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
-    # app.run(host='0.0.0.0', port=5001)
-    app.run()
+    app.run(host='0.0.0.0', debug=True, port=5001)
+    # app.run()
